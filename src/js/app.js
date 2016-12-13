b5268a593dff85bb118579dd3eb6c2771ee324c4
Jane Maguire <jane.m.maguire@gmail.com>
2016-12-13 20:39:04 +0000

    Started maps

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-angular.module('finalProject', ['ngResource', 'ui.router', 'satellizer'])
+angular.module('finalProject', ['ngResource', 'ui.router', 'satellizer', 'ui.map'])
   .constant('API_URL', 'http://localhost:3000/api')
   .config(Auth);
 
---
+++ src/js/directives/maps.js
@@ -0,0 +1,68 @@
+angular.module('finalProject')
+.directive('googleMap', googleMap);
+
+googleMap.$inject = ['$window'];
+function googleMap($window) {
+  return {
+    restrict: 'E',
+    replace: true,
+    template: '<div class="google-map"></div>',
+    scope: {
+      user: '='
+    },
+    link: function($scope, element) {
+      $scope.$watch('user', () => {
+        const myLatlng = new $window.google.maps.LatLng(parseFloat($scope.user.latitude),parseFloat($scope.user.longitude));
+        // const myLatlng = new $window.google.maps.LatLng(parseFloat(51.5395764),parseFloat(-0.1039495));
+        const map = new $window.google.maps.Map(element[0], {
+          center: myLatlng,
+          zoom: 10,
+          scrollwheel: false
+        });
+        const marker = new $window.google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          animation: $window.google.maps.Animation.DROP
+        });
+      });
+
+
+      // let markers = [];
+      // function clearMarkers() {
+      //   markers.forEach((marker) => {
+      //     marker.setMap(null);
+      //   });
+      //   markers = [];
+      // }
+
+      // $scope.$watch('spaces', () => {
+      //   clearMarkers();
+      //   if($scope.spaces.$resolved) {
+      //     $scope.spaces.forEach((space) => {
+      //       // console.log(space);
+      //       if(space.lat && space.lng) {
+              // const marker = new $window.google.maps.Marker({
+              //   position: myLatlng,
+              //   map: map,
+              //   animation: $window.google.maps.Animation.DROP
+              // });
+      //         markers.push(marker);
+      //         marker.addListener('click', () => {
+      //           // console.log(space);
+      //           const contentString = `
+      //           <img src=${space.image}>
+      //           <p>Contact: ${space.user.email}</p>`;
+      //
+      //           const infoWindow = new $window.google.maps.InfoWindow({
+      //             content: contentString
+      //           });
+      //           infoWindow.open(map, marker);
+      //           console.log('oi, that hurt');
+      //         });
+      //       }
+      //     });
+      //   }
+      // }, true);
+    }
+  };
+}
