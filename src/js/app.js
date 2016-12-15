8098ceb50793143e3003c5ab5bad7a2682fd83cb
Jane Maguire <jane.m.maguire@gmail.com>
2016-12-15 14:19:37 +0000

    Added masonry to index pages

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-angular.module('finalProject', ['ngResource', 'ui.router', 'satellizer', 'ui.map'])
+angular.module('finalProject', ['ngResource', 'ui.router', 'satellizer'])
   .constant('API_URL', 'http://localhost:3000/api')
   .config(Auth);
 
